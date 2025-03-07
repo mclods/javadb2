6c6e65ef000aea015fc1e58acac241d0882c64ac
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-03-08 00:47:58 +0530

    Added missing annotation

--- src/main/java/com/mclods/javadb2/repositories/AuthorRepository.java
+++ src/main/java/com/mclods/javadb2/repositories/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.mclods.javadb2.repositories;
 
 import com.mclods.javadb2.domain.Author;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
 }
